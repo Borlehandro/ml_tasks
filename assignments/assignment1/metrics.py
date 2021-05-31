7afcef179b9dbc5d2b5a55c43add1ba856fa62b9
Алексей Борзиков <a.borzikov@g.nsu.ru>
2021-06-01 00:58:32 +0700

    Complete task knn.

--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -1,5 +1,8 @@
+import numpy as np
+
+
 def binary_classification_metrics(prediction, ground_truth):
-    '''
+    """
     Computes metrics for binary classification
 
     Arguments:
@@ -8,22 +11,24 @@ def binary_classification_metrics(prediction, ground_truth):
 
     Returns:
     precision, recall, f1, accuracy - classification metrics
-    '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
+    """
+    true_positive = np.sum(prediction & ground_truth)
+    true_negative = np.sum(~prediction & ~ground_truth)
+    false_positive = np.sum(prediction & ~ground_truth)
+    false_negative = np.sum(~prediction & ground_truth)
 
-    # TODO: implement metrics!
-    # Some helpful links:
-    # https://en.wikipedia.org/wiki/Precision_and_recall
-    # https://en.wikipedia.org/wiki/F1_score
-    
-    return precision, recall, f1, accuracy
+    precision = true_positive / (true_positive + false_positive)
+    recall = true_positive / (true_positive + false_negative)
+
+    f1_metric = 2 * precision * recall / (precision + recall)
+    # correct / total
+    accuracy = (true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative)
+
+    return precision, recall, f1_metric, accuracy
 
 
 def multiclass_accuracy(prediction, ground_truth):
-    '''
+    """
     Computes metrics for multiclass classification
 
     Arguments:
@@ -32,6 +37,8 @@ def multiclass_accuracy(prediction, ground_truth):
 
     Returns:
     accuracy - ratio of accurate predictions to total samples
-    '''
-    # TODO: Implement computing accuracy
-    return 0
+    """
+    true_cnt = np.sum(prediction == ground_truth)
+    full_cnt = prediction.size
+
+    return true_cnt / full_cnt
